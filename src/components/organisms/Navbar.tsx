20e79653749675515a0e750b1f6c0252bca5f685
María Andrea <101602812+mariaa-mendez@users.noreply.github.com>
2025-03-29 15:19:27 -0500

    ✨ Feature (86a7dqbr3): Add new UI components and utilities for Landing page, and update styles

---
+++ src/components/organisms/Navbar.tsx
@@ -0,0 +1,52 @@
+import { useState } from "react";
+import { LogoIcon } from "../atoms/ui/icons";
+import { Button, buttonVariants } from "../atoms/ui/button";
+
+const routeList = [
+  { href: "#features", label: "Funcionalidades" },
+  { href: "#testimonials", label: "Testimonios" },
+];
+
+interface NavbarProps {
+  onCreateAccount: () => void;
+}
+
+export const Navbar = ({ onCreateAccount }: NavbarProps) => {
+  const [isOpen, setIsOpen] = useState(false);  return (
+    <header className="sticky top-0 z-40 w-full border-b border-orange-200 bg-white">
+      <nav className="flex items-center justify-between h-14 px-4 w-full max-w-[1400px] mx-auto">
+        <a href="/" className="flex items-center text-xl font-bold">
+          <LogoIcon />
+        </a>
+
+        {/* landing links */}
+        <div className="hidden md:flex space-x-4">
+          {routeList.map(({ href, label }) => (
+            <a key={href} href={href} onClick={() => setIsOpen(false)} className={buttonVariants({ variant: "ghost" })}>
+              {label}
+            </a>
+          ))}
+        </div>
+
+        {/* login and registry buttons */}
+        <div className="hidden md:flex items-center space-x-4">
+
+          <Button type="submit" variant="secondary" size="lg" onClick={onCreateAccount}>
+            Crear Cuenta
+          </Button>
+        </div>
+
+        {/* Mobile Menu */}
+        <button className="md:hidden p-2" onClick={() => setIsOpen(!isOpen)}>☰</button>
+      </nav>
+
+      {isOpen && (
+        <div className="md:hidden flex flex-col items-center">
+          {routeList.map(({ href, label }) => (
+            <a key={href} href={href} className="py-2">{label}</a>
+          ))}
+        </div>
+      )}
+    </header>
+  );
+};
--- src/routes/index.tsx
+++ src/routes/index.tsx
@@ -1,10 +1,11 @@
 // /src/routes/AppRoutes.tsx
 import React, { lazy, Suspense } from 'react'
-import { BrowserRouter, Routes, Route, Navigate } from 'react-router-dom'
+import { BrowserRouter, Routes, Route } from 'react-router-dom'
 
 // Carga perezosa (lazy) de componentes
 const Login = lazy(() => import('../components/screens/AuthScreen'))
 const TestCrud = lazy(() => import('../components/templates/TestCrud'))
+const Landing = lazy(() => import('../components/screens/LandingScreen'))
 
 export const AppRoutes = () => {
     return (
@@ -12,7 +13,7 @@ export const AppRoutes = () => {
             <Suspense fallback={<div>Cargando...</div>}>
                 <Routes>
                     {/* Redirige la raíz a /login */}
-                    <Route path="/" element={<Navigate to="/login" />} />
+                    <Route path="/" element={<Landing/>} />
 
                     {/* Ruta login */}
                     <Route path="/login" element={<Login />} />
